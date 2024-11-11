07f93fdcac9023dcb20490efc2a88b47377dfe1b
Arief Prabawa <81999477+Prabawa2222@users.noreply.github.com>
2024-11-11 19:02:25 +0700

    fix:unwanted response from creating new user

--- server/src/users/userController.js
+++ server/src/users/userController.js
@@ -1,196 +1,180 @@
-import * as bcrypt from "bcrypt"
+import * as bcrypt from "bcrypt";
 
-import User from "./usersModel.js"
+import User from "./usersModel.js";
 
 // Get users
-    export const getUsers = async (req, res) => {
-        try {
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.findAll(); // find all users
 
-            const users = await User.findAll()  // find all users
-
-            res.status(200).send({
-                success,
-                statusCode: 200,
-                users
-            })
-
-        } catch (error) {
-
-            res.status(500).send({
-                error,
-                status: 500,
-                message: 'Internal server error',
-            })
-
-        }
-    }
+    res.status(200).send({
+      success,
+      statusCode: 200,
+      users,
+    });
+  } catch (error) {
+    res.status(500).send({
+      error,
+      status: 500,
+      message: "Internal server error",
+    });
+  }
+};
 
 // Get user by Id
-    export const getUsersById = async (req, res) => {
-        try {
+export const getUsersById = async (req, res) => {
+  try {
+    const { id } = req.params.uid; // take request
 
-            const { id } = req.params.uid; // take request
+    const user = await User.findOne({
+      where: {
+        id,
+      },
+    });
 
-            const user = await User.findOne({
-                where: {
-                    id
-                }
-            })
-
-            if(!user) {
-                res.status(404).send({
-                    success,
-                    statusCode: 404,
-                    message: `User not found with ID : ${id}`
-                })    
-            }
-
-            res.status(200).send({
-                success,
-                statusCode: 200,
-                user
-            })
-
-        } catch (error) {
-
-            res.status(500).send({
-                error,
-                status: 500,
-                message: 'Internal server error',
-            })
-
-        }
+    if (!user) {
+      res.status(404).send({
+        success,
+        statusCode: 404,
+        message: `User not found with ID : ${id}`,
+      });
     }
+
+    res.status(200).send({
+      success,
+      statusCode: 200,
+      user,
+    });
+  } catch (error) {
+    res.status(500).send({
+      error,
+      status: 500,
+      message: "Internal server error",
+    });
+  }
+};
 
 // Delete user by Id
-    export const deleteUser = async (req, res) => {
-        try {
+export const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params.uid; // take request
 
-            const { id } = req.params.uid; // take request
+    const user = await User.destroy({
+      where: {
+        id,
+      },
+    });
 
-            const user = await User.destroy({
-                where: {
-                    id
-                }
-            })
-
-            if(!user) {
-                res.status(404).send({
-                    success,
-                    statusCode: 404,
-                    message: `User not found with ID : ${id}`
-                })    
-            }
-
-            res.status(200).send({
-                success,
-                statusCode: 200,
-                message: `User has been deleted successfully`
-            })
-
-        } catch (error) {
-
-            res.status(500).send({
-                error,
-                status: 500,
-                message: 'Internal server error',
-            })
-
-        }
+    if (!user) {
+      res.status(404).send({
+        success,
+        statusCode: 404,
+        message: `User not found with ID : ${id}`,
+      });
     }
 
-// Delete user by Id
-    export const createUser = async (req, res) => {
-        try {
+    res.status(200).send({
+      success,
+      statusCode: 200,
+      message: `User has been deleted successfully`,
+    });
+  } catch (error) {
+    res.status(500).send({
+      error,
+      status: 500,
+      message: "Internal server error",
+    });
+  }
+};
 
-            const { name, email, password } = req.body; // take request
+// Create user by Id
+export const createUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body; // take request
 
-            const existUser = await User.findOne({
-                where: {
-                    email: email
-                }
-            })
+    const existUser = await User.findOne({
+      where: {
+        email,
+      },
+    });
 
-            if(existUser) {
-                res.status(409).send({
-                    success,
-                    statusCode: 409,
-                    message: `Email has been taken !!`
-                })    
-            }
-
-            const salt = bcrypt.genSalt(5)
-            const hashedPassword = bcrypt.hash(password, salt)
-
-            const newUser = await User.create({
-                name,
-                email,
-                password: hashedPassword
-            })
-            
-            delete newUser.password
-
-            res.status(201).send({
-                success,
-                statusCode: 201,
-                message: `User has been created successfully`,
-                newUser
-            })
-
-        } catch (error) {
-
-            res.status(500).send({
-                error,
-                status: 500,
-                message: 'Internal server error',
-            })
-
-        }
+    if (existUser) {
+      res.status(409).send({
+        success,
+        statusCode: 409,
+        message: `Email has been taken !!`,
+      });
     }
+
+    const salt = await bcrypt.genSalt(5);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    delete newUser.password;
+
+    res.status(201).send({
+      success: true,
+      statusCode: 201,
+      message: `User has been created successfully`,
+      newUser: {
+        id: newUser.id,
+        name: newUser.name,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).send({
+      error,
+      status: 500,
+      message: "Internal server error",
+    });
+  }
+};
 
 // Update user by Id
-    export const updateUserById = async (req, res) => {
-        try {
+export const updateUserById = async (req, res) => {
+  try {
+    const { id } = req.params.uid; // take request
+    const { name, email, password } = req.body;
 
-            const { id } = req.params.uid; // take request
-            const { name, email, password } = req.body;
+    const user = await User.findOne({
+      where: {
+        id,
+      },
+    });
 
-            const user = await User.findOne({
-                where: {
-                    id
-                }
-            })
-
-            if(!user) {
-                res.status(404).send({
-                    success,
-                    statusCode: 404,
-                    message: `User not found with ID : ${id}`
-                })    
-            }
-
-            const newUser = await User.update(id, {
-                name,
-                email,
-                password
-            })
-
-            const newestUser = await User.create(newUser)
-
-            res.status(200).send({
-                success,
-                statusCode: 200,
-                message: `User has been updated successfully`,
-                newestUser
-            })
-
-        } catch (error) {
-
-            res.status(500).send({
-                error,
-                status: 500,
-                message: 'Internal server error',
-            })
-
-        }
+    if (!user) {
+      res.status(404).send({
+        success,
+        statusCode: 404,
+        message: `User not found with ID : ${id}`,
+      });
     }
+
+    const newUser = await User.update(id, {
+      name,
+      email,
+      password,
+    });
+
+    const newestUser = await User.create(newUser);
+
+    res.status(200).send({
+      success,
+      statusCode: 200,
+      message: `User has been updated successfully`,
+      newestUser,
+    });
+  } catch (error) {
+    res.status(500).send({
+      error,
+      status: 500,
+      message: "Internal server error",
+    });
+  }
+};
