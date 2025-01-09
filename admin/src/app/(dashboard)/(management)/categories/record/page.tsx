c9d5ee45e22ee583030fa40b8ec0bddb40bc329c
rach-syahputra <rach.syahputra@gmail.com>
2025-01-09 18:37:17 +0800

    feat(admin): add Category Record page

---
+++ admin/src/app/(dashboard)/(management)/categories/record/page.tsx
@@ -0,0 +1,10 @@
+import PageContainer from '@/components/container/page-container'
+import CategoryDataTable from './_components/table/category-data-table'
+
+export default function UsersRecordPage() {
+  return (
+    <PageContainer>
+      <CategoryDataTable />
+    </PageContainer>
+  )
+}
