3333a06511f32bd622e96039607f55bbac399ed3
rach-syahputra <rach.syahputra@gmail.com>
2025-01-09 18:36:59 +0800

    feat(admin): add CategoryDataTable component

---
+++ admin/src/app/(dashboard)/(management)/categories/record/_components/table/category-columns.tsx
@@ -0,0 +1,115 @@
+'use client'
+
+import { MoreHorizontal } from 'lucide-react'
+
+import { ColumnDef } from '@tanstack/react-table'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { Button } from '@/components/ui/button'
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type CategoryTable = {
+  id: string
+  title: string
+  createdAt: string
+}
+
+export const categoryColumns: ColumnDef<CategoryTable>[] = [
+  // ROW SELECTION
+  // {
+  //   id: 'select',
+  //   header: ({ table }) => (
+  //     <Checkbox
+  //       checked={
+  //         table.getIsAllPageRowsSelected() ||
+  //         (table.getIsSomePageRowsSelected() && 'indeterminate')
+  //       }
+  //       onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+  //       aria-label='Select all'
+  //     />
+  //   ),
+  //   cell: ({ row }) => (
+  //     <Checkbox
+  //       checked={row.getIsSelected()}
+  //       onCheckedChange={(value) => row.toggleSelected(!!value)}
+  //       aria-label='Select row'
+  //     />
+  //   ),
+  //   enableSorting: false,
+  //   enableHiding: false
+  // },
+  {
+    accessorKey: 'title',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='table-header'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className='px-0 text-xs lg:text-sm'
+        >
+          Title
+        </Button>
+      )
+    }
+  },
+  {
+    accessorKey: 'createdAt',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='table-header'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className='px-0 text-xs lg:text-sm'
+        >
+          Created At
+        </Button>
+      )
+    }
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const category = row.original
+
+      return (
+        <div className='flex w-full items-center justify-center'>
+          <DropdownMenu>
+            <DropdownMenuTrigger className='border' asChild>
+              <Button variant='ghost' className='h-8 w-8 p-0'>
+                <span className='sr-only'>Open menu</span>
+                <MoreHorizontal className='h-4 w-4' />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align='end'>
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() => navigator.clipboard.writeText(category.id)}
+              >
+                Copy category ID
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem asChild>
+                {/* <UserDetailModal user={row.original} /> */}
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                {/* <EditUserModal user={row.original} /> */}
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem asChild className='text-destructive'>
+                {/* <DeleteUserModal id={row.original.id} /> */}
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      )
+    }
+  }
+]
---
+++ admin/src/app/(dashboard)/(management)/categories/record/_components/table/category-data-table.tsx
@@ -0,0 +1,30 @@
+import { formatDateForTable } from '@/lib/utils'
+import { ICategoryJson } from '@/lib/types/json'
+import { fetchAllCategory } from '@/lib/api/services'
+import { DataTable } from '@/components/table/data-table'
+import { categoryColumns, CategoryTable } from './category-columns'
+
+async function getData(): Promise<CategoryTable[]> {
+  const res: ICategoryJson[] = await fetchAllCategory()
+
+  return res.map((category) => ({
+    ...category,
+    createdAt: formatDateForTable(new Date(category.createdAt))
+  }))
+}
+
+const CategoryDataTable = async () => {
+  const data = await getData()
+
+  const searchableColumns: string[] = ['title', 'createdAt']
+
+  return (
+    <DataTable
+      columns={categoryColumns}
+      data={data}
+      searchableColumns={searchableColumns}
+    />
+  )
+}
+
+export default CategoryDataTable
