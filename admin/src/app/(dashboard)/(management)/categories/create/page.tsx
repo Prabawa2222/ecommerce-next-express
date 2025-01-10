3f0a520e64708d47560c3d3cefac131ee207df66
rach-syahputra <rach.syahputra@gmail.com>
2025-01-10 16:47:24 +0800

    feat(admin): add Create Category page

---
+++ admin/src/app/(dashboard)/(management)/categories/create/page.tsx
@@ -0,0 +1,10 @@
+import PageContainer from '@/components/container/page-container'
+import CreateCategoryForm from './_components/create-category-form'
+
+export default function CreateProductPage() {
+  return (
+    <PageContainer>
+      <CreateCategoryForm />
+    </PageContainer>
+  )
+}
