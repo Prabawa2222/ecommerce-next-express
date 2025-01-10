a8d31eca60308b67287938e027c27f8c7ef5ab92
rach-syahputra <rach.syahputra@gmail.com>
2025-01-10 17:16:23 +0800

    feat(admin): add OrderPieChart component to display order status totals

---
+++ admin/src/app/(dashboard)/(management)/orders/overview/_components/order-pie-chart.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import { fetchAllOrder } from '@/lib/api/services'
+import { IOrderJson } from '@/lib/types/json'
+import DataPieChart, { IChartData } from '@/components/chart/pie-chart'
+
+type OrderPieChartProps = {
+  className?: string
+}
+
+const OrderPieChart = ({ className }: OrderPieChartProps) => {
+  const [orderChartData, setOrderChartData] = useState<IChartData[]>([])
+
+  const getOrderChartData = async () => {
+    const orders: IOrderJson[] = await fetchAllOrder()
+
+    const orderAmountMap: Record<string, number> = {}
+
+    orders.forEach((order) => {
+      if (order) {
+        orderAmountMap[order.status] = (orderAmountMap[order.status] || 0) + 1
+      }
+    })
+
+    const chartData: IChartData[] = Object.entries(orderAmountMap).map(
+      ([status, amount]) => ({
+        label: status,
+        amount
+      })
+    )
+
+    setOrderChartData(chartData)
+  }
+
+  useEffect(() => {
+    getOrderChartData()
+  }, [])
+
+  return (
+    <DataPieChart
+      title='Order Status'
+      nameKey='status'
+      chartData={orderChartData}
+      className={className}
+    />
+  )
+}
+
+export default OrderPieChart
