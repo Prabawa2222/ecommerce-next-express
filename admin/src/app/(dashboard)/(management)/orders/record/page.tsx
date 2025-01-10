091df99e34c2ad578a71920efdcab33b7bcef019
rach-syahputra <rach.syahputra@gmail.com>
2025-01-10 17:35:57 +0800

    feat(admin): add OrderRecord page

---
+++ admin/src/app/(dashboard)/(management)/orders/record/page.tsx
@@ -0,0 +1,10 @@
+import PageContainer from '@/components/container/page-container'
+import OrderDataTable from './_components/table/order-data-table'
+
+export default function OrderRecordPage() {
+  return (
+    <PageContainer>
+      <OrderDataTable />
+    </PageContainer>
+  )
+}
